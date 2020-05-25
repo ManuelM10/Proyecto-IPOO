06f1313353ae03fd4630a0812dc72c9c0252d0a6
IngeniJuan <62144599+IngeniJuan@users.noreply.github.com>
2020-05-25 09:01:34 -0500

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -20,7 +20,7 @@ class Tablero{
     void mostrarTablero();//muestra el tablero y posicion de las fichas
     void guardarTablero();//guarda la ultima partida jugada
     int tablero[10][10];//Array que representa el tablero
-    void cargarPartida(string rutaArchivo);
+    
     void modificarElemento(int x, int y, int valor);//modifica un punto del tablero.
     void mostrarInstrucciones(string a);
 
